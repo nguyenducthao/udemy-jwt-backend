102ee49c779f8761c147ededcf1430c0ac57da04
nguyenducthao <ndthaodng@gmail.com>
2022-04-18 16:41:29 +0700

    video 05 Create new user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,32 @@
+import mysql from "mysql2"
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'udemy-jwt-backend'
+})
 const handleHelloWord = (req, res) => {
     return res.render("home.ejs")
 }
 const handleUser = (req, res) => {
     return res.render("user.ejs")
 }
+const handleCreateNewUser = (req, res) => {
+    // return res.render("user.ejs")
+    let email = req.body.email
+    let password = req.body.password
+    let username = req.body.username
+    connection.query(
+        'insert into users (email,password,username) values (?,?,?)', [email, password, username],
+        function (err, result, fields) {
+            if (err)
+                console.log(err)
+        }
+    )
+    return res.send('handleCreateNewUser')
+}
 module.exports = {
     handleHelloWord,
-    handleUser
+    handleUser,
+    handleCreateNewUser
 }
--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config
-
+import bodyParser from "body-parser"
 const app = express()
 configViewEngine(app)
+
+//config body-parser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 initWebRoutes(app)
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
