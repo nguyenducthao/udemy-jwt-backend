84244cbc08dc7420c1d6e146cc48430a3561a6a1
nguyenducthao <ndthaodng@gmail.com>
2022-04-19 14:35:45 +0700

    video 5.3 Delete Users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,18 +4,34 @@ const handleHelloWord = (req, res) => {
     return res.render("home.ejs")
 }
 const handleUserPage = async (req, res) => {
-    let userList = await userService.getUserList()
-    return res.render("user.ejs", { userList })
+    try {
+        let userList = await userService.getUserList()
+        return res.render("user.ejs", { userList })
+    } catch (e) {
+        console.log(e)
+    }
+
 }
-const handleCreateNewUser = (req, res) => {
+const handleCreateNewUser = async (req, res) => {
+    let getUserByEmail = await userService.getUserByEmail('admin@gmail.com')
+    if (getUserByEmail) {
+        alert('This email is exist')
+        return
+    }
     let email = req.body.email
     let password = req.body.password
     let username = req.body.username
-    userService.createNewUser(email, password, username)
-    return res.send('handleCreateNewUser')
+    await userService.createNewUser(email, password, username)
+    // return res.send('handleCreateNewUser')
+    return res.redirect('/user')
+}
+const handleDeleteUser = async (req, res) => {
+    await userService.deleteUser(req.params.id)
+    return res.redirect('/user')
 }
 module.exports = {
     handleHelloWord,
     handleUserPage,
-    handleCreateNewUser
+    handleCreateNewUser,
+    handleDeleteUser
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 import mysql from "mysql2/promise"
 import bcrypt from "bcryptjs"
 import bluebird from 'bluebird'
@@ -8,24 +9,47 @@ import bluebird from 'bluebird'
 //     password: 'root',
 //     database: 'udemy-jwt-backend'
 // })
-
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_DATABASE = process.env.DB_DATABASE
 const hashUserPassword = (password) => {
     const salt = bcrypt.genSaltSync(10)
     const hashPassword = bcrypt.hashSync(password, salt)
     return hashPassword
 }
 
-const createNewUser = (email, password, username) => {
+const createNewUser = async (email, password, username) => {
     let hashPass = hashUserPassword(password)
-    connection.query(
-        'insert into users (email,password,username) values (?,?,?)', [email, hashPass, username],
-        function (err, result, fields) {
-            if (err)
-                console.log(err)
-        }
-    )
+    // connection.query(
+    //     'insert into users (email,password,username) values (?,?,?)', [email, hashPass, username],
+    //     function (err, result, fields) {
+    //         if (err)
+    //             console.log(err)
+    //     }
+    // )
+    const connection = await mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE,
+        Promise: bluebird
+    });
+    await connection.execute('insert into users (email,password,username) values (?,?,?)', [email, hashPass, username])
 }
 const getUserList = async () => {
+    const connection = await mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE,
+        Promise: bluebird
+    });
+
+    const [rows, fields] = await connection.execute('SELECT * FROM users')
+    return rows
+}
+const getUserByEmail = async (email) => {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
@@ -34,10 +58,22 @@ const getUserList = async () => {
         Promise: bluebird
     });
 
-    const [rows, fields] = await connection.execute('SELECT * FROM users');
+    const [rows, fields] = await connection.execute('SELECT * FROM users where email=?', [email])
     return rows
+}
+const deleteUser = async (id) => {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'udemy-jwt-backend',
+        Promise: bluebird
+    });
+    await connection.execute('delete FROM users where id=?', [id])
 }
 module.exports = {
     createNewUser,
-    getUserList
+    getUserList,
+    getUserByEmail,
+    deleteUser
 }
