75c3469d12632683c5c56df63bf25024cfb293d7
nguyenducthao <ndthaodng@gmail.com>
2022-04-23 10:28:41 +0700

    Tao API de goi tu frontend

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from './/../controllers/homeController'
+import apiController from "../controllers/apiController";
 const router = express.Router()
 
 const initWebRoutes = (app) => {
@@ -9,6 +10,7 @@ const initWebRoutes = (app) => {
     router.post("/users/delete-user/:id", homeController.handleDeleteUser)
     router.get("/users/update-user/:id", homeController.getUpdateUserPage)
     router.post("/users/update-user", homeController.handleUpdateUser)
+    router.get("/api/test-api", apiController.testApi)
     return app.use("/", router)
 }
 export default initWebRoutes
