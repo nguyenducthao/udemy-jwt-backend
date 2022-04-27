8fd616f1750f345c83ef48958b2444a95db63c58
nguyenducthao <ndthaodng@gmail.com>
2022-04-27 10:18:40 +0700

    video 10.5 Base User CRUD - Code Chuan RESTful APIs

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,17 @@
 import express from "express";
 import apiController from "../controllers/apiController";
+import userController from '../controllers/userController'
+
 const router = express.Router()
 
 const initApiRoutes = (app) => {
     router.get("/test-api", apiController.testApi)
     router.post("/register", apiController.handleRegister)
     router.post("/login", apiController.handleLogin)
+    router.get("/users/read", userController.readFunc)
+    router.post("/users/create", userController.createFunc)
+    router.put("/users/update", userController.updateFunc)
+    router.delete("/users/delete", userController.deleteFunc)
     return app.use("/api/v1/", router)
 }
 export default initApiRoutes
