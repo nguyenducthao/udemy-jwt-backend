30575e5c162a91ff7814d2293c3fd37eb95a1483
nguyenducthao <ndthaodng@gmail.com>
2022-04-28 10:34:27 +0700

    Modal Create User - Tao Form Them Moi User

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 import express from "express";
 import apiController from "../controllers/apiController";
 import userController from '../controllers/userController'
+import groupController from '../controllers/groupController'
 
 const router = express.Router()
 
@@ -13,6 +14,8 @@ const initApiRoutes = (app) => {
     router.post("/users/create", userController.createFunc)
     router.put("/users/update", userController.updateFunc)
     router.delete("/users/delete", userController.deleteFunc)
+    router.get("/group/read", groupController.readFunc)
+
     return app.use("/api/v1/", router)
 }
 export default initApiRoutes
---
+++ src/services/groupService.js
@@ -0,0 +1,27 @@
+import db from '../models/index'
+
+const getGroups = async () => {
+    try {
+        let data = await db.Group.findAll({
+            order: [
+                ['name', 'ASC']
+            ]
+        })
+        return {
+            EM: 'get group success',
+            EC: 0,
+            DT: data
+        }
+    } catch (e) {
+        console.log(e)
+        return {
+            EM: 'Something wrongs with services',
+            EC: 1,
+            DT: []
+        }
+    }
+}
+
+module.exports = {
+    getGroups,
+}
