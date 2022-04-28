134769056a601aba44979d7782e2a319b9de2a16
nguyenducthao <ndthaodng@gmail.com>
2022-04-28 08:05:27 +0700

    Delete Users - Xaa Ngưoi Dung

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -59,7 +59,12 @@ const updateFunc = async (req, res) => {
 }
 const deleteFunc = async (req, res) => {
     try {
-        let users = await userApiService.deleteUser()
+        let data = await userApiService.deleteUser(req.body.id)
+        return res.status(200).json({
+            EM: data.EM, //error message
+            EC: data.EC,//error code
+            DT: data.DT //data
+        })
     } catch (e) {
         console.log(e)
         return res.status(500).json({
--- src/services/userApiService.js
+++ src/services/userApiService.js
@@ -32,6 +32,8 @@ const getUserWithPagination = async (page, limit) => {
     try {
         let offset = (page - 1) * limit
         const { count, rows } = await db.User.findAndCountAll({
+            attributes: ["id", "username", "email", "phone", "sex"],
+            include: { model: db.Group, attributes: ["name", "description"] },
             offset: offset,
             limit: limit
         })
@@ -91,9 +93,23 @@ const updateUser = async (data) => {
 }
 const deleteUser = async (id) => {
     try {
-        await db.User.delete({
+        let user = await db.User.findOne({
             where: { id: id }
         })
+        if (user) {
+            await user.destroy()
+            return {
+                EM: 'Delete user success',
+                EC: 0,
+                DT: []
+            }
+        } else {
+            return {
+                EM: 'User not exist',
+                EC: 2,
+                DT: []
+            }
+        }
     } catch (e) {
         console.log(e)
         return {
